72ab2724262cd0a339c2767b6c79315195ef266f
Shaun Tung <shaun.tung@gmail.com>
2021-09-16 16:13:40 +0800

    pokemon search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { ReactQueryDevtools } from "react-query-devtools";
+
 import axios from "axios";
 
 export default function App() {
+  const [pokemon, setPokemon] = React.useState("");
   return (
     <div>
-      <Pokemon />
-      <Berries />
+      <input value={pokemon} onChange={(e) => setPokemon(e.target.value)} />
+      <PokemonSearch pokemon={pokemon} />
       <ReactQueryDevtools />
     </div>
   );
 }
 
-function Berries() {
-  const queryInfo = useQuery("berries", async () => {
+function PokemonSearch({ pokemon }) {
+  const queryInfo = useQuery(pokemon, async () => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     return axios
-      .get("https://pokeapi.co/api/v2/berry")
-      .then((res) => res.data.results);
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((res) => res.data);
   });
 
   return queryInfo.isLoading ? (
@@ -27,34 +29,13 @@ function Berries() {
     queryInfo.error.message
   ) : (
     <div>
-      {queryInfo.data.map((result) => {
-        return <div key={result.name}>{result.name}</div>;
-      })}
+      {queryInfo.data?.sprites?.front_default ? (
+        <img src={queryInfo.data.sprites.front_default} alt="pokemon" />
+      ) : (
+        "Pokemon not found."
+      )}
       <br />
-      {queryInfo.isFetching && "Updating..."}
-    </div>
-  );
-}
-
-function Pokemon() {
-  const queryInfo = useQuery("pokemons", async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return axios
-      .get("https://pokeapi.co/api/v2/pokemon")
-      .then((res) => res.data.results);
-  });
-
-  return queryInfo.isLoading ? (
-    "Loading..."
-  ) : queryInfo.isError ? (
-    queryInfo.error.message
-  ) : (
-    <div>
-      {queryInfo.data.map((result) => {
-        return <div key={result.name}>{result.name}</div>;
-      })}
-      <br />
-      {queryInfo.isFetching && "Updating..."}
+      {queryInfo.isFetching ? "Updating..." : null}
     </div>
   );
 }
