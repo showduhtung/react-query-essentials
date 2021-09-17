c998b6ddce37f72ccc4f3844ab47a3bbe69385ea
Shaun Tung <shaun.tung@gmail.com>
2021-09-17 15:23:41 +0800

    prefetches

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,49 +2,46 @@ import React, { useEffect, useReducer } from 'react'
 import { queryCache, useQuery } from 'react-query'
 import axios from 'axios'
 
+const fetchPosts = async () => {
+  await new Promise(resolve => setTimeout(resolve, 1000))
+  return axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.data.slice(0, 10))
+}
+
 export default function App() {
+  const [show, toggle] = useReducer(d => !d, false)
+  useEffect(() => {
+    queryCache.prefetchQuery('posts', fetchPosts)
+  }, [])
   return (
     <div>
-      <button onClick={() => queryCache.invalidateQueries('random')}>
-        Invalidate Random Number
-      </button>
-      <button
-        onClick={() => queryCache.invalidateQueries(['random', 'A'])}
-      >
-        Invalidate A
-      </button>
-      <button
-        onClick={() => queryCache.invalidateQueries(['random', 'B'])}
-      >
-        Invalidate B
-      </button>
-      <button
-        onClick={() => queryCache.invalidateQueries(['random', 'C'])}
-      >
-        Invalidate C
-      </button>
-      <RandomNumber subKey="A" />
-      <RandomNumber subKey="B" />
-      <RandomNumber subKey="C" />
+      <button onClick={toggle}>{!show ? 'Show' : 'Hide'} Posts</button>
+      {show && <Posts />}
     </div>
   )
 }
 
-function RandomNumber({ subKey }) {
-  const randomQuery = useQuery(
-    ['random', subKey],
-    async () => axios.get('/api/random').then(res => res.data),
-    { staleTime: Infinity }
-  )
-
+function Posts({ setPostId }) {
+  const postsQuery = useQuery('posts', fetchPosts)
   return (
     <div>
-      <h1>Random Number {randomQuery.isFetching && '...'}</h1>
-      <h2>
-        {randomQuery.isLoading
-          ? 'Loading random number...'
-          : Math.round(randomQuery.data.random * 1000)}
-      </h2>
+      <h1>Posts {postsQuery.isFetching && '...'}</h1>
+      <div>
+        {postsQuery.isLoading ? (
+          'Loading posts...'
+        ) : (
+          <ul>
+            {postsQuery.data.map(post => (
+              <li key={post.id}>
+                <a onClick={() => setPostId(post.id)} href="#">
+                  {post.title}{' '}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   )
 }
